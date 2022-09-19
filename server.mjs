cae38362ecedf240f58497c57994c85502ec6bfa
khurrammustaqeem <itzkhurram367@gmail.com>
2022-09-20 00:35:36 +0500

    myFirstServer

---
+++ server.mjs
@@ -0,0 +1,11 @@
+import express from "express";
+const server = express();
+const port = process.evn.PORT || 3000;
+server.get("/", (req, res) => {
+  console.log(`someone is looking at u`);
+  res.send(`I LOVE YOU YUMNA`)
+});
+
+server.listen(port, () => {
+  console.log(`App listning on port ${port}`);
+});
